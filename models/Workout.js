93416d043495c1eab97cba1b23b3c9d883535e17
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2020-03-26 10:40:50 -0400

    fixed

--- models/Workout.js
+++ models/Workout.js
@@ -2,52 +2,53 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutData = new Schema({
-  day: {
-    type: Date,
-    default: () => new Date()
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: "Please enter excercise type"
-      },
-      name: {
-        type: String,
-        trim: true,
-        required: "Please enter a name"
-      },
-      duration: {
-        type: Number,
-        required: "please enter a duration"
-      },
-      weight: {
-        type: Number
-      },
-      reps: {
-        type: Number
-      },
-      sets: {
-        type: Number
-      },
-      distance: {
-        type: Number
+const workoutData = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Please enter excercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Please enter a name"
+        },
+        duration: {
+          type: Number,
+          required: "please enter a duration"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
       }
-    }
-  ]
-},
-{
-   toJSON:{
+    ]
+  },
+  {
+    toJSON: {
       virtuals: true
-   }
-}
+    }
+  }
 );
-workoutData.virtual("totalDuration").get(function (){
-   return this.exercises.reduce((total, excercise)=>{
-      return total = excercise.duration
-   }, 0)
+workoutData.virtual("totalDuration").get(function() {
+  return this.exercises.reduce((total, excercise) => {
+    return total + excercise.duration;
+  }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutData);
